61b30a73301e3d8399528cf26ec5d47ffa07304c
Harold <bhflores1994@gmail.com>
2021-04-26 10:21:20 -0500

    Fixing unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { LargeButtonComponent } from './large-button/large-button.component';
 import { MediumButtonComponent } from './medium-button/medium-button.component';
 import { NumberBoxComponent } from './number-box/number-box.component';
-import { SmallButtonComponent } from './small-button/small-button.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        SmallButtonComponent,
         MediumButtonComponent,
-        LargeButtonComponent,
         NumberBoxComponent
 
       ],
